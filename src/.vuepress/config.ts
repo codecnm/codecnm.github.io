d9474e4f8dd7ab911347450a741a8b5b834cc845
lihairui <2698457486@qq.com>
2024-02-29 16:44:11 +0800

    修改base配置

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/hope/", // 默认为 '/' 现在修改为 '/hope/'以适应GitHub Paes页面访问路径
 
   lang: "zh-CN",
   title: "博客演示",
